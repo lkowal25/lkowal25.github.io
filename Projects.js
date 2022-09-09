6cd48903f03a53cadb028452f3a55f15d69ff22f
lkowal25 <leland.kowal@gmail.com>
2022-09-08 21:20:27 -0400

    pics?

---
+++ Projects.js
@@ -0,0 +1,14 @@
+const projects = [
+  {
+    'Hello World': {
+      techStack: ['HTML5', 'CSS'],
+      dateCompleted: '6/1/22',
+    },
+  },
+  {
+    'Second Project': {
+      techStack: ['CSS', 'React', 'Redux', 'Blah'],
+      dateCompleted: '7/2/22',
+    },
+  },
+];
